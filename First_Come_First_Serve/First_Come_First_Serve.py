81669cf634e4d8646994bea594ea9784b8249ea8
Haleshot <hari.leo03@gmail.com>
2022-08-27 19:53:08 +0530

    Updated

--- First_Come_First_Serve/First_Come_First_Serve.py
+++ First_Come_First_Serve/First_Come_First_Serve.py
@@ -38,47 +38,69 @@ def Waiting_Time(P, limit):
 def Logic(P, limit):
     completion_time = 0
     exit_time = []
-
     
-
+    for i in range(limit):
+        if completion_time < P[i][1]:
+            completion_time = P[i][1]
+        completion_time += P[i][2]
+        exit_time.append(completion_time)
+        P[i].append(completion_time)
     
-    
+    tat = Turn_Around_Time(P, limit)
+    wt = Waiting_Time(P, limit)
+
+    P.sort(key=sorting_arrival) # Sorting the List by Arrivak Time
+    headers = ["Process Number", "Arrival Time", "Burst Time", "Completion Time", "Turn Around Time", "Waiting Time"]
+    print(tabulate(P, headers, tablefmt="psql"))
+
+    # Printing the Average Waiting and Turn Around Time
+    print("\nAverage Waiting Time is = ", round(wt, 2)) # Rounding off Average Waiting Time to 2 Decimal places
+    print("Average Turn Around Time is = ", round(tat, 2)) # Rounding off Average Turn Around Time to 2 Decimal places
 
 
 
+# Main Function
 def main():
     run = True
     while(run):
-        
+
         # Declaring arrays
-        arrival_time = []
-        burst_time = []
-        limit_process = int(input("Enter the Number of Processes : "))
-        
-        print("\nDo you want to assume : \n1. Arrival Time as 0\n2. Input Arrival Time\n3. Exit\n")
+        processes = []
+
+        print("\nMenu\nDo you want to assume : \n1. Arrival Time as 0\n2. Input Arrival Time\n3. Exit\n")
         ch = int(input("Enter Your Choice : "))
 
         if ch == 1:
+            limit_process = int(input("Enter the Number of Processes : "))
             for i in range(limit_process):
+                p = []
                 arrival = 0
                 burst = int(input("Enter the Burst Time for process {} : ".format(i)))
-                arrival_time.append(arrival)
-                burst_time.append(burst)
+                process_id = "P" + str(i + 1)
 
-            Logic(arrival_time, burst_time, limit_process)
+                p.extend([process_id, arrival, burst])
+                processes.append(p)
+
+            Logic(processes, limit_process)
             run = int(input("\nWant to continue? (Yes = Input 1/false = Input 0) : "))
 
         elif ch == 2:
+            limit_process = int(input("Enter the Number of Processes : "))
             for i in range(limit_process):
+                p = []
                 arrival = int(input("Enter the Arrival Time for process {} : ".format(i)))
                 burst = int(input("Enter the Burst Time for process {} : ".format(i)))
-                arrival_time.append(arrival)
-                burst_time.append(burst)
+                process_id = "P" + str(i + 1)
 
-            Logic(arrival_time, burst_time, limit_process)
+                p.extend([process_id, arrival, burst])
+                processes.append(p)
+                
+
+            Logic(processes, limit_process)
             run = int(input("\nWant to continue? (Yes = Input 1/false = Input 0) : "))
         
         elif ch == 3:
+            print("Thank You!")
             exit(0)
 
         else:
@@ -86,5 +108,5 @@ def main():
             run = int(input("\nWant to continue? (Yes = Input 1/false = Input 0) : "))
 
 
-
+# Calling the Main Function
 main()
